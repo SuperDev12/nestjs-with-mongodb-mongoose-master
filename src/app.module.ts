bd4f897680f87bca48393968feba35545f8c9bd7
Dev Rathore <94687428+SuperDev12@users.noreply.github.com>
2024-06-05 14:42:40 +0530

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017'),
+    UsersModule,
+    PostsModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
